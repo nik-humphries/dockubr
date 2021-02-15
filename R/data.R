e87ee490ca6af37c6dbe5c47e484d716ef3318f7
Nicholas Humphries <NHumphries@seamsglobal.com>
2021-02-15 13:03:27 +0000

    Added documentation for the dataset

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Dependency List
+#'
+#' @format A tibble with 3 columns and many records:
+#' \describe{
+#'   \item{package}{Package Name}
+#'   \item{dependency}{Actual code for the dependency}
+#'   \item{type}{Type of bash code to install}
+#' }
+"dep_list"
